442eb801d32b7a7d1e17c38790075258fa114c74
Enrico casa <enricopw100@gmail.com>
2024-05-09 10:19:06 -0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  
+
   {
     path: 'cartas',
     loadChildren: () =>
     import('./cartas/cartas.module').then((m) => m.CartasModule),
   },
+  {
+    path:'jogador',
+    loadChildren: () =>
+      import('./jogador/jogador.module').then((m) =>m.JogadorModule),
+  }
 ];
---
+++ src/app/jogador/jogador-listagem/jogador-listagem.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jogador-listagem',
+  templateUrl: './jogador-listagem.component.html',
+  styleUrl: './jogador-listagem.component.scss'
+})
+export class JogadorListagemComponent implements OnInit {
+
+  constructor() {  }
+
+  ngOnInit(): void {
+  }
+}
